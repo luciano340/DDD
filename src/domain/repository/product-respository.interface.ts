c8e9a3f56b78525bfe83f206f7e1231ea058f979
Luciano Romao <lucianosouzaromao@gmail.com>
2023-08-11 08:53:28 -0300

    ajuste nos diretórios e primeiros repositórios

---
+++ src/domain/repository/product-respository.interface.ts
@@ -0,0 +1,6 @@
+import RepositoryInterface from "./repository-interface";
+import Product from "../entity/products";
+
+export default interface ProductRepositoryInterface extends RepositoryInterface<Product> {
+
+}
